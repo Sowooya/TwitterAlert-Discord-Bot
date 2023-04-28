6d327fdf20953da4068f5f3b51660f23bf7e4176
ZsomborBalatoni <zsombor.balatoni@gmail.com>
2023-04-28 19:38:57 +0200

    Adding files

---
+++ src/main/java/com/zsomborbalatoni/TwitterAlertDiscordBot/TwitterAlertDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package com.zsomborbalatoni.TwitterAlertDiscordBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAlertDiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAlertDiscordBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zsomborbalatoni/TwitterAlertDiscordBot/TwitterAlertDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsomborbalatoni.TwitterAlertDiscordBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAlertDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
